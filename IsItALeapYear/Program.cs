3e3727a9d8249f2880a2779c35b62f7fe6cefc42
iedirisi <65867578+iedirisi@users.noreply.github.com>
2021-08-12 18:16:44 -0400

    Add files via upload

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Boolean IsItALeapYear(int year)
+        {
+            if(year%4==0)
+            {
+                if(year%100==0)
+                {
+                    if(year%400==0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+           return false;
+        }
+    }
+}
